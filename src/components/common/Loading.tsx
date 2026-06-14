7c133a9b0b229552d052e33e693577c8b8d2bbbc
rlaugs15 <68183848+rlaugs15@users.noreply.github.com>
2026-06-14 20:03:35 +0900

    refactor: 로딩 컴포넌트 변경

--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -1,15 +1,19 @@
-import loadingAnimation from "@/assets/loadingAnimation.json";
 import { cn } from "@/lib/utils";
-import Lottie from "lottie-react";
 
 interface LoadingProps {
   className?: string;
 }
 
+{
+  /* <div className={cn("flex items-center justify-center", className)}>
+      <Lottie animationData={loadingAnimation} loop={true} />
+    </div> */
+}
+
 export default function Loading({ className }: LoadingProps) {
   return (
-    <div className={cn("flex items-center justify-center", className)}>
-      <Lottie animationData={loadingAnimation} loop={true} />
+    <div className={cn("flex h-full w-full items-center justify-center", className)}>
+      <div className="h-8 w-8 animate-spin rounded-full border-4 border-gray-200 border-t-blue-500" />
     </div>
   );
 }
